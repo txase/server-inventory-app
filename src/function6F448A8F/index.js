9b2ed4c6cb10a9e8764b7ff7d51d9b82a7ac3ab7
Chase Douglas <chase@stackery.io>
2018-08-30 16:37:39 -0700

    Bot function

--- src/function6F448A8F/index.js
+++ src/function6F448A8F/index.js
@@ -1,5 +1,65 @@
-exports.handler = async message => {
-  console.log(message);
+module.exports.handler = async message => {
+  const body = JSON.parse(message.body.toString());
 
-  return {};
-}
+  if (body.token != process.env.SLACK_VERIFICATION_TOKEN) {
+    throw new Error('Invalid Verification Token received from Slack!');
+  }
+
+  switch (body.type) {
+    case 'url_verification':
+      // Respond to the initial verification challenge when first setting
+      // up integration with Slack
+      return {
+        body: body.challenge
+      };
+
+    case 'event_callback':
+      if (body.event.type !== 'app_mention') {
+        throw new Error(`Unrecognized callback type: ${body.event.type}`);
+
+      return handleMention(body.event);
+
+    default:
+      throw new Error(`Unrecognized event type: ${body.type}`);
+  }
+};
+
+const KILL_RE = /kill (([a-zA-Z0-9_]+[,\s]+)*[a-zA-Z0-9_]+)/;
+const handleMention = async event => {
+  const killMatch = event.text.match(KILL_RE);
+
+  if (killMatch) {
+    const serversToKill = killMatch[1].split(/[,\s]+/);
+
+    await db()('servers')
+      .whereIn('name', serversToKill)
+      .delete();
+  } else if (event.text.includes('killall')) {
+    await db()('servers')
+      .delete();
+  }
+};
+
+const postMessage = async message => {
+  const fetch = require('node-fetch');
+
+  await fetch('https://slack.com/api/chat.postMessage', {
+    method: 'POST',
+    body: JSON.stringify(message),
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_BOT_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json',
+    }
+  });
+};
+
+const db = () => require('knex')({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_ADDRESS,
+    port: process.env.DB_PORT,
+    user: 'root',
+    password: process.env.DB_PASSWORD
+  }
+});
+
