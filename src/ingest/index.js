6a6e2894d533ab57db2bff63dbf4f69656854838
Chase Douglas <chase@stackery.io>
2018-08-31 08:30:58 -0700

    Ingest code

--- src/ingest/index.js
+++ src/ingest/index.js
@@ -1,5 +1,11 @@
-exports.handler = async message => {
-  console.log(message);
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda();
 
-  return {};
-}
+exports.handler = async message => {
+  await lambda.invoke({
+    FunctionName: process.env.FUNCTION_NAME,
+    InvocationType: 'Event',
+    Payload: JSON.stringify(message)
+  }).promise();
+};
+
